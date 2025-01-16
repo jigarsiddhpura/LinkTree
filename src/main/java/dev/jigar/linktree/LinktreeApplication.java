ba0bb3bec422df1aba532206e45defece711291a
Jigar <95428432+jigarsiddhpura@users.noreply.github.com>
2025-01-16 12:22:04 +0530

    init

---
+++ src/main/java/dev/jigar/linktree/LinktreeApplication.java
@@ -0,0 +1,13 @@
+package dev.jigar.linktree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinktreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinktreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jigar/linktree/LinktreeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jigar.linktree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinktreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
